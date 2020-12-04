8df5eb18cf8a8be9a05ff019ceb7da0e83d1723b
member <Kaz-furniture>
2020-12-04 20:15:30 +0900

    bottom navigation 表示されず

--- app/src/main/java/com/kaz_furniture/mahjongChat/activity/MainActivity.kt
+++ app/src/main/java/com/kaz_furniture/mahjongChat/activity/MainActivity.kt
@@ -3,20 +3,38 @@ package com.kaz_furniture.mahjongChat.activity
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.kaz_furniture.mahjongChat.R
 import com.kaz_furniture.mahjongChat.activity.base.BaseActivity
 
 class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 
 
     companion object {
         fun start(activity: Activity) =
-                activity.apply {
-                    startActivity(Intent(activity, MainActivity::class.java))
-                }
+            activity.apply {
+                startActivity(Intent(activity, MainActivity::class.java))
+            }
     }
 }
---
+++ app/src/main/java/com/kaz_furniture/mahjongChat/ui/home/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.kaz_furniture.mahjongChat.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.kaz_furniture.mahjongChat.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+
+}
