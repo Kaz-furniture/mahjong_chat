f4af876e1f27fdb840bffe21d64f6b7a2a0c6de4
yss_miura <jane.was.gentleman@gmail.com>
2020-12-06 18:08:50 +0900

    魔改造

--- app/src/main/java/com/kaz_furniture/mahjongChat/activity/MainActivity.kt
+++ app/src/main/java/com/kaz_furniture/mahjongChat/activity/MainActivity.kt
@@ -3,16 +3,14 @@ package com.kaz_furniture.mahjongChat.activity
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.kaz_furniture.mahjongChat.R
 import com.kaz_furniture.mahjongChat.activity.base.BaseActivity
 import com.kaz_furniture.mahjongChat.databinding.ActivityMainBinding
@@ -26,35 +24,24 @@ class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-        binding = DataBindingUtil.setContentView(
-            this,
-            R.layout.activity_main
-        )
-        setSupportActionBar(binding.toolBar)
-        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
-        binding.drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-//        appBarConfiguration = AppBarConfiguration(setOf(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-//        ), binding.drawerLayout)
-
         binding.bottomNuv.setupWithNavController(navController)
-        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
-//        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_home, R.id.navigation_second, R.id.navigation_third), binding.drawerLayout)
+        binding.navView.setupWithNavController(navController)
         setupActionBarWithNavController(navController, appBarConfiguration)
-        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        //return navController.navigateUp()
-        return NavigationUI.navigateUp(navController,appBarConfiguration) || super.onSupportNavigateUp()
+    //bottom nav
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) ||
+                super.onOptionsItemSelected(item)
+    }
 
+    //open drawer when drawer icon clicked and back btn presse
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
     }
 
 
