0f42b21ae28e04dc9c19b4ffb21100186fd36ece
yss_miura <jane.was.gentleman@gmail.com>
2021-01-12 22:03:16 +0900

    modify

--- app/src/main/java/com/kaz_furniture/mahjongChat/viewModel/DMDetailViewModel.kt
+++ app/src/main/java/com/kaz_furniture/mahjongChat/viewModel/DMDetailViewModel.kt
@@ -1,6 +1,15 @@
 package com.kaz_furniture.mahjongChat.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.ListenerRegistration
 
 class DMDetailViewModel: ViewModel() {
+
+    private var listenerRegistration: ListenerRegistration? = null
+
+
+    override fun onCleared() {
+        super.onCleared()
+        listenerRegistration?.remove()
+    }
 }
